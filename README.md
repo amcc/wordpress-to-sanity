dd2767c74b76da6981fe0f29c42023a83186cd1e
amcc <alistairmcclymont@gmail.com>
2023-03-25 14:57:36 +0000

    feat: tweaks to make export work on custom posts and newer wp and sanity

--- README.md
+++ README.md
@@ -7,13 +7,20 @@ You most probably have to tweak and edit the code to fit to your need.
 ## Getting started
 
 1. Clone this repo
-2. Set `const filename` in `migrate.js` to the path of your wordpress export xml file
-3. Run `npm start` to log out the converted sanity documents in the terminal
-4. Pipe the output to an import file by `npm start > myImportfile.ndjson`
-5. Try to import the file with `sanity dataset import myImportfile.ndjson` in your Sanity project folder
+2. Set `const postType` at the top of `migrate.js` for custom post types
+3. Set `const filename` in `migrate.js` to the path of your wordpress export xml file
+4. Run `node src/migrate.js` to log out the converted sanity documents in the terminal
+5. Pipe the output to an import file by `node src/migrate.js > myImportfile.ndjson`
+6. Try to import the file with `sanity dataset import myImportfile.ndjson` in your Sanity project folder
 
 Mostly probably there is additional content that this script doesn't migrate, however, it should cover the most usual patterns, so that you can build it out for your specific use case.
 
+## some helpful tips
+
+- the bug described below means that if there's an issue you'll get nothing generated
+- console log stuff to check the output before you go
+- there's a counter variable so you can just test a single post
+- liberal use of optional chaining will help you! i.e. `?.` on objects
 
 - This script reads the wordpress export XML file
 - This example is based on the blog template schema
@@ -21,4 +28,8 @@ Mostly probably there is additional content that this script doesn't migrate, ho
 - Debugging deserialization (`parseBody.js`) is easier with the script in `/test`
 - Remember to add additional supported custom types to `/schemas/defaultSchema.js`
 - The HTML is sanitized, but _most_ HTML tags are allowed (check `/lib/sanitizeHTML.js`)
-- 
+
+## to do
+
+- update various dependencies to work as well as possible with sanity v.3
+- https://www.npmjs.com/package/@sanity/block-tools this package does the heavy lifting for blocks which is the only really difficult part of this. Currently formatting isn't quite right.
--- src/migrate.js
+++ src/migrate.js
@@ -2,27 +2,31 @@
 
 /* eslint-disable id-length, no-console, no-process-env, no-sync, no-process-exit */
 const fs = require('fs')
-const { log } = console
+const {log} = console
 const XmlStream = require('xml-stream')
 const parseDate = require('./lib/parseDate')
 const parseBody = require('./lib/parseBody')
 const slugify = require('slugify')
-function generateAuthorId (id) {
+
+// set the required post type
+const postType = 'post'
+
+function generateAuthorId(id) {
   return `author-${id}`
 }
 
-function generateCategoryId (id) {
+function generateCategoryId(id) {
   return `category-${id}`
 }
 
-function readFile (path = '') {
+function readFile(path = '') {
   if (!path) {
     return console.error('You need to set path')
   }
   return fs.createReadStream(path)
 }
 
-async function buildJSONfromStream (stream) {
+async function buildJSONfromStream(stream) {
   const xml = await new XmlStream(stream)
 
   return new Promise((res, rej) => {
@@ -30,7 +34,7 @@ async function buildJSONfromStream (stream) {
      * Get some meta info
      */
     const meta = {}
-    xml.on('text: wp:base_site_url', url => {
+    xml.on('text: wp:base_site_url', (url) => {
       meta.rootUrl = url.$text
     })
 
@@ -38,12 +42,12 @@ async function buildJSONfromStream (stream) {
      * Get the categories
      */
     const categories = []
-    xml.on('endElement: category', wpCategory => {
-      const { nicename } = wpCategory.$
+    xml.on('endElement: category', (wpCategory) => {
+      const {nicename} = wpCategory.$
       const category = {
         _type: 'category',
         _id: generateCategoryId(nicename),
-        title: nicename
+        title: nicename,
       }
       categories.push(category)
     })
@@ -52,15 +56,15 @@ async function buildJSONfromStream (stream) {
      * Get the users
      */
     const users = []
-    xml.on('endElement: wp:author', author => {
+    xml.on('endElement: wp:author', (author) => {
       const user = {
         _type: 'author',
         _id: generateAuthorId(author['wp:author_id']),
         name: author['wp:author_display_name'],
         slug: {
-          current: slugify(author['wp:author_login'], { lower: true })
+          current: slugify(author['wp:author_login'], {lower: true}),
         },
-        email: author['wp:author_email']
+        email: author['wp:author_email'],
       }
       users.push(user)
     })
@@ -68,58 +72,85 @@ async function buildJSONfromStream (stream) {
     /**
      * Get the posts
      */
+    let count = 0
     const posts = []
     xml.collect('wp:postmeta')
-    xml.on('endElement: item', item => {
-      const { title, category, link: permalink, description } = item
-      if (item['wp:post_type'] != 'post' && item['wp:post_type'] != 'page') { return }
+    xml.on('endElement: item', (item) => {
+      const {title, category, link, description} = item
+
+      // use original permalink
+      const removeSlash = link?.endsWith('/') ? link.slice(0, -1) : link
+      const slug = removeSlash?.split('/').pop()
+
+      const itemPostType = item['wp:post_type']
+      if (itemPostType !== postType) return
+      if (count === 0) {
+        // useful to log out one single item
+        // console.log(item)
+      }
       const post = {
-        _type: 'post',
+        _type: postType,
+        _createdAt: parseDate(item),
         title,
         slug: {
-          current: slugify(title, { lower: true })
+          current: slug,
         },
-        categories: [
+        tags: [
           {
-            _type: 'reference',
-            _ref: generateCategoryId(category.$.nicename)
-          }
+            value: 'tag',
+            _ref: generateCategoryId(category?.$?.nicename),
+          },
         ],
         description,
-        body: parseBody(item['content:encoded']),
-        publishedAt: parseDate(item)
-        /* author: {
+        body: parseBody(
+          item['wp:postmeta'].find((item) => item['wp:meta_key'] === 'story')['wp:meta_value']
+        ),
+        publishedAt: parseDate(item),
+        author: {
           _type: 'reference',
-          _ref: users.find(user => user.slug.current === item['dc:creator'])._id
+          _ref: users.find((user) => user.slug.current === item['dc:creator'])._id,
         },
-        */
       }
+      // useful logging options
+
+      // log all posts before the output
+      // console.log(post)
+
+      // log out a specific post
+      // if (count === 100) posts.push(post)
+
+      // log out the first post
+      // if (count === 0) console.log(post)
+
+      count++
+
+      // add to the array
       posts.push(post)
     })
 
     // there seems to be a bug where errors is not caught
-    xml.on('error', err => {
+    xml.on('error', (err) => {
       throw new Error(err)
     })
 
     xml.on('end', () => {
       const output = [
         /* meta, */
-        ...users,
+        // ...users,
         ...posts,
-        ...categories
+        // ...categories,
       ]
-
+      // console.log(output)
       return res(output)
     })
   })
 }
 
-async function main () {
-  const filename = './exampleData/alexoglou.xml'
+async function main() {
+  const filename = './wordpress-export.xml'
   const stream = await readFile(filename)
   const output = await buildJSONfromStream(stream)
-  output.forEach(doc => log(JSON.stringify(doc, null, 0)))
+  output.forEach((doc) => log(JSON.stringify(doc, null, 0)))
 }
 
 main()
